d9c2e80e060c33a40607a45e7e5f9a5b6f04b3b7
Melissa Freitas <81538721+melissa-mfs@users.noreply.github.com>
2023-03-14 14:40:43 -0300

    estrutura ds classe jsonUtil

--- VioletGames/Util/Clean/Clean.cs
+++ VioletGames/Util/Clean/Clean.cs
@@ -11,5 +11,25 @@ namespace VioletGames.Util.Clean
         {
             return dateForClean.Replace("00:00:00", "");
         }
+
+        //Criar limpeza de json
+        public static void jsonItemClean(){
+            //tentar com um new itemPedido();
+            ItemPedidoModel itemPedido = JsonUtil.jsonItemDeserialize();
+
+            JsonUtil.jsonItemSerialize(itemPedido);
+        }
+
+        public static void jsonCaixaClean(){
+            CaixaModel caixa = JsonUtil.jsonCaixaDeserialize();
+
+            JsonUtil.jsonCaixaSerialize(caixa);
+        }
+
+        public static void jsonItensClean(){
+            List<Object> itensPedido = JsonUtil.jsonItensDeserialize();
+
+            JsonUtil.jsonItensSerialize(itensPedido);
+        }
     }
 }
