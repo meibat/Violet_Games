fe1371994aba1c2a1dcce7badeb3559af9555837
Melissa Freitas <81538721+melissa-mfs@users.noreply.github.com>
2023-03-28 13:35:03 -0300

    Add filtro dos consoles no dash

--- VioletGames/wwwroot/js/site.js
+++ VioletGames/wwwroot/js/site.js
@@ -98,3 +98,20 @@ function getDatatableCaixa(id) {
         }
     });
 }
+
+// https://codepen.io/bolzan/pen/YmeZpz
+function aplicaFiltroCards() {
+    var input, filter, cards, cardContainer, h5, title, i;
+    input = document.getElementById("filtroCards");
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById("containerCards");
+    cards = cardContainer.getElementsByClassName("card");
+    for (i = 0; i < cards.length; i++) {
+        title = cards[i].querySelector(".card-body");
+        if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
