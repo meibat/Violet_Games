72d70aeb3d656001c0a66e284a88f15b6675f8dc
Melissa Freitas <melissa.mf29@gmail.com>
2023-04-28 23:23:23 -0300

    ajustes no caixa e parte escrita

--- VioletGames/Data/Repositorio/CaixaRepositorio.cs
+++ VioletGames/Data/Repositorio/CaixaRepositorio.cs
@@ -160,7 +160,7 @@ namespace VioletGames.Data.Repositorio
                             _bancoContent.Produtos.Update(produto);//Atualiza a quantidade do produto
                             _bancoContent.SaveChanges();
                         }
-                        if(item.NameProduct.Substring(0,7) == "Locação")
+                        else if(item.NameProduct.Substring(0,7) == "Locação")
                         {
                             string[] palavras = item.NameProduct.Split(" ");
 
@@ -173,7 +173,7 @@ namespace VioletGames.Data.Repositorio
                             _bancoContent.Agendamentos.Update(agendamento);//Atualiza o status do agendamento
                             _bancoContent.SaveChanges();
                         }
-                        if (item.NameProduct.Substring(0, 5) == "Plano")
+                        else if (item.NameProduct.Substring(0, 5) == "Plano")
                         {
                             //Update Plano para pago
                             string[] palavras = item.NameProduct.Split(" ");
